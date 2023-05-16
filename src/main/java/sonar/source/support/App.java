cced826925cbfba1b677ecf5ab216ce76c243744
Michal Budzowski <michal.budzowski@sonarsource.com>
2023-05-16 16:05:40 +0200

    add issue

--- src/main/java/sonar/source/support/App.java
+++ src/main/java/sonar/source/support/App.java
@@ -10,4 +10,9 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    
+    int myPow(int num, int exponent) {
+        num = num * myPow(num, exponent - 1); // Noncompliant
+        return num;  // this is never reached
+    }
 }
