edfa11a3ffe5eea9473f4e75cde1e64c420d96b5
User Created on 7.9 <user-created-on-7.9@sonar.org>
2023-09-06 09:09:24 +0200

    new lines added in App.java in feature1

--- src/main/java/sonar/source/support/App.java
+++ src/main/java/sonar/source/support/App.java
@@ -1,13 +1,23 @@
 package sonar.source.support;
 
+import java.math.BigDecimal;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private long computationId;
+	private String marginTypeKey;
+	private BigDecimal cashMarginAmount;
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+	
+	public static String[] getColumnNames() {
+		return new String[1];
+	}
 }
